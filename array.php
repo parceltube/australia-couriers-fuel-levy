1a3bdd28658c22cd72da6ff4bc21de782760af16
Norman <support@parceltube.com.au>
2024-08-01 12:32:56 +1000

    Update array.php
    for augues

--- array.php
+++ array.php
@@ -131,6 +131,39 @@ $array=array("2024" => array(
      "08" =>array(
                 array("company" => "australia_post","product" => "australia_post","fuel" => "7.3","extra" => "0"),
                 array("company" => "australia_post","product" => "startrack","fuel" => "7.3","extra" => "0"),
+                array("company" => "aramex","product" => "myfastway","fuel" => "9","extra" => "0"),
+                array("company" => "toll","product" => "priority","fuel" => "9.82","extra" => "0"),
+                array("company" => "toll","product" => "ipec","fuel" => "9.55","extra" => "0"),
+                array("company" => "toll","product" => "pallets","fuel" => "15.2","extra" => "0"),
+                array("company" => "oc","product" => "ocep","fuel" => "0","extra" => "0"),
+                array("company" => "couriers_please","product" => "couriers_please","fuel" => "29.5","extra" => "9"),
+                array("company" => "sendle","product" => "sendle","fuel" => "8.8","extra" => "0"),
+                array("company" => "hunter_express","product" => "hunter_express","fuel" => "20","extra" => "0"),
+                array("company" => "allied_express","product" => "sameday_local","fuel" => "18.9","extra" => "21.1"),
+                array("company" => "allied_express","product" => "interstate","fuel" => "27.9","extra" => "0"),
+                array("company" => "border_express","product" => "border_express","fuel" => "23","extra" => "0"),
+                array("company" => "direct_freight","product" => "direct_freight","fuel" => "22","extra" => "0"),
+    ),
+    "09" =>array(
+               array("company" => "australia_post","product" => "australia_post","fuel" => "7.4","extra" => "0"),
+               array("company" => "australia_post","product" => "startrack","fuel" => "7.4","extra" => "0"),
+               array("company" => "aramex","product" => "myfastway","fuel" => "8.6","extra" => "0"),
+               array("company" => "toll","product" => "priority","fuel" => "10.82","extra" => "0"),
+               array("company" => "toll","product" => "ipec","fuel" => "10.4","extra" => "0"),
+               array("company" => "toll","product" => "pallets","fuel" => "16.56","extra" => "0"),
+               array("company" => "oc","product" => "ocep","fuel" => "0","extra" => "0"),
+               array("company" => "couriers_please","product" => "couriers_please","fuel" => "28.7","extra" => "9.7"),
+               array("company" => "sendle","product" => "sendle","fuel" => "8.8","extra" => "0"),
+               array("company" => "hunter_express","product" => "hunter_express","fuel" => "20","extra" => "0"),
+               array("company" => "allied_express","product" => "sameday_local","fuel" => "18.9","extra" => "21.1"),
+               array("company" => "allied_express","product" => "interstate","fuel" => "27.9","extra" => "0"),
+               array("company" => "border_express","product" => "border_express","fuel" => "23","extra" => "0"),
+               array("company" => "direct_freight","product" => "direct_freight","fuel" => "22","extra" => "0"),
+   )	
+   ,
+     "10" =>array(
+                array("company" => "australia_post","product" => "australia_post","fuel" => "7.3","extra" => "0"),
+                array("company" => "australia_post","product" => "startrack","fuel" => "7.3","extra" => "0"),
                 array("company" => "aramex","product" => "myfastway","fuel" => "8.9","extra" => "0"),
                 array("company" => "toll","product" => "priority","fuel" => "10.82","extra" => "0"),
                 array("company" => "toll","product" => "ipec","fuel" => "10.4","extra" => "0"),
@@ -143,7 +176,7 @@ $array=array("2024" => array(
                 array("company" => "allied_express","product" => "interstate","fuel" => "27.9","extra" => "0"),
                 array("company" => "border_express","product" => "border_express","fuel" => "23","extra" => "0"),
                 array("company" => "direct_freight","product" => "direct_freight","fuel" => "22","extra" => "0"),
-    )	
+    )		
   )		
 
 );
